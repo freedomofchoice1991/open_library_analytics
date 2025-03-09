c0f7419901db030f31d19f1fac1f24fe7063ef88
REZA <reza.karami.arbeiten@gmail.com>
2025-03-09 12:09:50 +0100

    Bill Gates

--- data_pipeline/generate_report.py
+++ data_pipeline/generate_report.py
@@ -5,7 +5,7 @@ def save_to_csv(book_data, filename="book_report.csv"):
     """Saves processed book data to a CSV file."""
     keys = ["title", "author", "first_publish_year", "isbn"]
 
-    with open(filename, "a", newline="", encoding="utf-8") as file:
+    with open(filename, "w", newline="", encoding="utf-8") as file:
         writer = csv.DictWriter(file, fieldnames=keys)
         writer.writeheader()
         writer.writerows(book_data)
@@ -17,6 +17,6 @@ if __name__ == "__main__":
     from process_data import process_books
     from fetch_data import fetch_books_by_author
 
-    books = fetch_books_by_author("J.K. Rowling", 8)
+    books = fetch_books_by_author("Bill Gates", 8)
     processed_books = process_books(books)
     save_to_csv(processed_books)
